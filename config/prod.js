da98dacb6892bd3c2f0f3026e83bc401fa31587a
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-09-22 22:20:53 -0400

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,12 @@
 // prod.js - production keys here!
 
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  googleClientID: "451488821164-ug7i9c67ggp06ss2u334p636011hql7j.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb+srv://yd999:Draze999@sendup.dhq2yho.mongodb.net/?retryWrites=true&w=majority",
+  cookieKey: "MnJWaWoZYh8aotZhrqM4W1xOmwnDNnst",
+  stripePublishableKey: "pk_live_51MggAIL1YxZP4XjbUXItCjvNFLeMXlP5K1v88zDPswmnil54uM62fv24nYdYh36rM24mRNgDsgsnhTokboXPWop800dJ4kEo2P",
+  stripeSecretKey: "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",
+  sendGridKey: "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",
+  redirectDomain: "https://sendup-opal.vercel.app/login"
 };
